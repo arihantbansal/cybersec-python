c9844235d8342c5af4ab198ef44078ac84a013a5
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-07-23 10:50:12 +0530

    Added JS render text

--- Web Scraping/rhtml_demo.py
+++ Web Scraping/rhtml_demo.py
@@ -1,9 +1,10 @@
 from requests_html import HTML, HTMLSession
 import csv
 
-# with open("simple.html") as html_file:
-# 	source = html_file.read()
-# 	html = HTML(html=source)
+with open("simple.html") as html_file:
+	source = html_file.read()
+	html = HTML(html=source)
+	html.render()
 
 # print(html.text)
 
@@ -26,34 +27,40 @@ import csv
 # 	summary = article.find("p", first=True)
 # 	print(headline.text + "\n" + summary.text + "\n")
 
-csv_file = open("cms_scrape.csv", "w")
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["headline", "summary", "video"])
+match = html.find("#footer", first=True)
+print(match.html)
 
-session = HTMLSession()
-r = session.get("https://coreyms.com/")
+# csv_file = open("cms_scrape.csv", "w")
+# csv_writer = csv.writer(csv_file)
+# csv_writer.writerow(["headline", "summary", "video"])
+
+# session = HTMLSession()
+# r = session.get("https://coreyms.com/")
+
+# for link in r.html.absolute_links:
+# 	print(link)
 
 # print(r.html)
 
-articles = r.html.find("article")
+# articles = r.html.find("article")
 
-for article in articles:	
-	headline = article.find(".entry-title-link", first=True).text
-	print(headline)
+# for article in articles:	
+# 	headline = article.find(".entry-title-link", first=True).text
+# 	print(headline)
 
-	summary = article.find(".entry-content p", first=True).text
-	print(summary)
+# 	summary = article.find(".entry-content p", first=True).text
+# 	print(summary)
 
-	try:
-		vid_src = article.find("iframe", first=True).attrs["src"]
-		vid_id = vid_src.split("/")[4].split("?")[0]
-		yt_link = f'https://youtube.com/watch?v={vid_id}'
-	except Exception as e:
-		yt_link = None	
+# 	try:
+# 		vid_src = article.find("iframe", first=True).attrs["src"]
+# 		vid_id = vid_src.split("/")[4].split("?")[0]
+# 		yt_link = f'https://youtube.com/watch?v={vid_id}'
+# 	except Exception as e:
+# 		yt_link = None	
 	
-	print(yt_link)
-	print()
+# 	print(yt_link)
+# 	print()
 
-	csv_writer.writerow([headline, summary, yt_link])
+# 	csv_writer.writerow([headline, summary, yt_link])
 
-csv_file.close()
+# csv_file.close()
