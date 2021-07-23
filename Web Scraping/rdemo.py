21a493b19391510e80adf70894945f4172ed16a0
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-07-23 10:34:12 +0530

    vid src found

--- Web Scraping/rdemo.py
+++ Web Scraping/rdemo.py
@@ -1,30 +1,30 @@
 import requests
 
-# r = requests.get("https://imgs.xkcd.com/comics/python.png")
+r = requests.get("https://imgs.xkcd.com/comics/python.png")
 
-# print(r)
-# print(r.text)
-# print(r.content)
+print(r)
+print(r.text)
+print(r.content)
 
-# with open("comic.png", "wb") as f:
-# 		f.write(r.content)
+with open("comic.png", "wb") as f:
+		f.write(r.content)
 
-# print(r.status_code)
-# print(r.ok)
-# print(r.headers)
+print(r.status_code)
+print(r.ok)
+print(r.headers)
 
-# payload = {"page": 2, "count": 25}
-# r = requests.get("https://httpbin.org/get", params=payload)
+payload = {"page": 2, "count": 25}
+r = requests.get("https://httpbin.org/get", params=payload)
 
-# print(r.text)
-# print(r.url)
+print(r.text)
+print(r.url)
 
-# r = requests.post("https://httpbin.org/post", data={"username": "ariban900", "password": "123456"})
+r = requests.post("https://httpbin.org/post", data={"username": "ariban900", "password": "123456"})
 
-# # print(r.text)
+print(r.text)
 
-# r_dict = r.json()
-# print(r_dict)
+r_dict = r.json()
+print(r_dict)
 
 r = requests.get("https://httpbin.org/basic-auth/ariban900/123456", auth=("ariban900", "123456"))
 
---
+++ Web Scraping/rhtml_demo.py
@@ -0,0 +1,47 @@
+from requests_html import HTML, HTMLSession
+
+# with open("simple.html") as html_file:
+# 	source = html_file.read()
+# 	html = HTML(html=source)
+
+# print(html.text)
+
+# match = html.find("title", first=True)
+# print(match[0].html)
+# print(match.text)
+
+# match = html.find("#footer", first=True)
+# print(match.text)
+
+# article = html.find("div.article", first=True)
+# # print(article.text)
+# headline = article.find("h2", first=True)
+# summary = article.find("p", first=True)
+# print(headline.text + "\n" + summary.text)
+
+# articles = html.find("div.article")
+# for article in articles:
+# 	headline = article.find("h2", first=True)
+# 	summary = article.find("p", first=True)
+# 	print(headline.text + "\n" + summary.text + "\n")
+
+session = HTMLSession()
+r = session.get("https://coreyms.com/")
+
+# print(r.html)
+
+article = r.html.find("article", first=True)
+# print(article.html)
+
+# headline = article.find(".entry-title-link", first=True).text
+# print(headline)
+
+# summary = article.find(".entry-content p", first=True).text
+# print(summary)
+
+vid_src = article.find("iframe", first=True).attrs["src"]
+vid_id = vid_src.split("/")[4].split("?")[0]
+# print(vid_id)
+
+yt_link = f'https://youtube.com/watch?v={vid_id}'
+print(yt_link)
