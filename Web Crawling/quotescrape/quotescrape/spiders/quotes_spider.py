f5a02ce5dc2b1af8092b4e88c7334c60a55ec12f
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-07-28 16:34:06 +0530

    minor

--- Web Crawling/quotescrape/quotescrape/spiders/quotes_spider.py
+++ Web Crawling/quotescrape/quotescrape/spiders/quotes_spider.py
@@ -3,15 +3,10 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
   name = "quotes"
-
-  def start_requests(self):
-    start_urls = {
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
-    }
-
-    for url in start_urls:
-      yield scrapy.Request(url=url, callback=self.parse)
+  start_urls = {
+      'http://quotes.toscrape.com/page/1/',
+      'http://quotes.toscrape.com/page/2/',
+  }
 
   def parse(self, response):
     page = response.url.split("/")[-2]
