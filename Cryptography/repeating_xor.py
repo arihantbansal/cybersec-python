133dcb1aed5345460cfa8c6efac5dee78ddf7e3d
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-08-13 21:26:48 +0530

    Chall 5 solved

---
+++ Cryptography/repeating_xor.py
@@ -0,0 +1,14 @@
+from tqdm import tqdm  # For progress bar
+from binascii import unhexlify, hexlify
+
+
+def encode_xor(msg, key):
+  return bytes([msg[i] ^ key[i % len(key)] for i in tqdm(range(len(msg)))])
+
+
+message = input("Enter your message: ").encode()
+key = input("Enter key: ").encode()
+
+ciphertext = encode_xor(message, key)
+print("-" * 140)
+print("Ciphertext: ", hexlify(ciphertext))
